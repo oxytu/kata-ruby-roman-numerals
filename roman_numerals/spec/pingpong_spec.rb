6c668d3374a1cf9463e2ca86a4c8c9ac5fe6a34b
Markus Bruegner <maho.b@web.de>
2012-11-03 15:22:55 +0100

    Commented a puts statement in an RSpec example

--- roman_numerals/spec/pingpong_spec.rb
+++ roman_numerals/spec/pingpong_spec.rb
@@ -7,7 +7,7 @@ describe "pingpong test" do
     9999.times do |i|
       r = dec_to_roman(i + 1)
       d = i + 1
-      puts "#{r} == #{d} ?"
+      #puts "#{r} == #{d} ?"
       roman_to_dec(r).should eql(d)
     end
   end
